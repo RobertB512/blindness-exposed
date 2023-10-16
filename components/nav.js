0451488ce8aa6bf1186d2ed46c147108aa247012
RobertB512 <rwbcool@gmail.com>
2023-10-16 11:44:55 -0700

    Add html content to new page, add temp placeholder image

--- components/nav.js
+++ components/nav.js
@@ -6,7 +6,7 @@ class Nav extends HTMLElement {
 		this.innerHTML = `
     <nav class="top-nav">
     <ul class="nav-link-list">
-      <li><a href="">Home</a></li>
+      <li><a href="../index.html">Home</a></li>
       <li><a href="./pages/aboutBlindness.html">About blindness</a></li>
       <li><a href="">Blindness questions</a></li>
       <li><a href="">Tools for the blind</a></li>
